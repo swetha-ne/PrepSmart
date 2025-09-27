b018438a8fc34a9fac5640072d5e87ac97302c99
Swetha Nethmi <snethmi18@gmail.com>
2025-09-27 22:54:18 +0530

    feat: connect mongo db

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { connect } = require("http2");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -13,7 +15,15 @@ app.use(cors(
     }
 ));
 
+connectDB();
 app.use(express.json());
+
+app.use("/api/auth",authRoutes);
+// app.use("/api/sessions",sessionRoutes);
+// app.use("/api/questions",questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect,generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect,generateConceptExplanation);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"),{}));
 
